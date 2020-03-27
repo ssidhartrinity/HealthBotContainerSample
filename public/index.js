df1d1b1860d48ca16a9f9b7d01a8b7da696e9c3e
ssidhartrinity <62565794+ssidhartrinity@users.noreply.github.com>
2020-03-27 09:23:46 -0400

    changes

--- public/index.js
+++ public/index.js
@@ -77,20 +77,18 @@ function initBotConversation() {
         token: tokenPayload.connectorToken,
         domain: domain
     });
-    function fontFamily(fonts) {
-        return fonts.map(font => `'${font}'`).join(', ');
-      }
+  
       
     const styleOptions = {
-        avatarBorderRadius: '50%',
-        avatarSize: 40,
-        botAvatarBackgroundColor: '#555659',
+       // avatarBorderRadius: '50%',
+       // avatarSize: 40,
+      //  botAvatarBackgroundColor: '#555659',
        // botAvatarImage: 'https://media-exp1.licdn.com/dms/image/C4E0BAQEclA3Vh3sTNw/company-logo_200_200/0?e=2159024400&v=beta&t=KD7qFl36K5BdZmGGqu1k8uZD-wYIn47CW_nR5f0l1z4',
-         botAvatarInitials: 'C',
+        // botAvatarInitials: 'C',
         // userAvatarImage: '',
         hideSendBox: true, /* set to true to hide the send box from the view */
         //botAvatarInitials: 'C',
-        userAvatarInitials: 'You',
+       // userAvatarInitials: 'You',
         backgroundColor: '#F8F8F8',
 }
 
