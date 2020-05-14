aaab681f0ad44e9691b49fb04579b2defe1341b9
ssidhartrinity <62565794+ssidhartrinity@users.noreply.github.com>
2020-05-14 12:02:42 -0400

    changes

--- public/index.js
+++ public/index.js
@@ -85,9 +85,9 @@ function initBotConversation() {
         botAvatarBackgroundColor: '#555659',
        // botAvatarImage: 'https://media-exp1.licdn.com/dms/image/C4E0BAQEclA3Vh3sTNw/company-logo_200_200/0?e=2159024400&v=beta&t=KD7qFl36K5BdZmGGqu1k8uZD-wYIn47CW_nR5f0l1z4',
         // userAvatarImage: '',
-        hideSendBox: False, /* set to true to hide the send box from the view */
-        botAvatarInitials: 'C',
-        userAvatarInitials: 'You',
+        hideSendBox: True, /* set to true to hide the send box from the view */
+       // botAvatarInitials: 'C',
+       // userAvatarInitials: 'You',
         backgroundColor: '#F8F8F8',
 }
 
